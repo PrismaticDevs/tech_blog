96dd0252fe6747df6f58bcead5769b0baebb3f42
Matthew Brignola <matthewbrignola@ymail.com>
2022-02-13 21:28:08 -0700

    built comment file but need to create view toutes and add fetch request for controlller

---
+++ public/js/createComment.js
@@ -0,0 +1,25 @@
+let $comment_text = document.querySelector('#comment_text')
+let $comment_submit = document.querySelector('#comment_submit');
+
+async function createComment(e) {
+    e.preventDefault();
+    try {
+        const response = await fetch("/comments/create", {
+            method: "post",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+
+            //make sure to serialize your JSON body
+            body: JSON.stringify({
+                text: $comment_text.value,
+            })
+        })
+        $comment_text.value = '';
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+$comment_submit.addEventListener('click', createComment);
