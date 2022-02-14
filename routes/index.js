83e37241d6b10c827cc6f4f6c734dc0d00a730de
Matthew Brignola <matthewbrignola@ymail.com>
2022-02-14 16:18:57 -0700

    my posts working and view adjusted to who uuername

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./apiRoutes');
+const { getMyPosts } = require('../controllers/PostsController');
+
 
 router.use('/', apiRoutes);
+router.get('/myPosts', getMyPosts);
+
 
 module.exports = router;
